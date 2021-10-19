c2824743cdaab1ffc05b0852936ff0d9bb230a75
Ayana_moooomin <51011031+RilaKorila@users.noreply.github.com>
2021-10-19 17:44:00 +0900

    Add files via upload

---
+++ js/calendar.js
@@ -0,0 +1,84 @@
+const week = ["日", "月", "火", "水", "木", "金", "土"]
+const today = new Date()
+var year = today.getFullYear()
+var month = today.getMonth() + 1
+
+function creatCalendarTitle(year, month) {
+    return year + "/" + month
+}
+
+function creatCalendar(year, month) {
+    // 月の最初の日を取得
+    const firstDate = new Date(year, month - 1, 1)
+    // 月の最初の曜日
+    const firstDay = firstDate.getDay()
+    // 月の最後の日を取得
+    const lastDate = new Date(year, month, 0)
+    const lastDayCount = lastDate.getDate()
+    // 日にちのカウント
+    var dayCount = 1
+    // HTML要素を格納する変数
+    var calendarHtml = ""
+
+    calendarHtml += "<table>"
+
+    // 曜日の行を作成
+    for (let i = 0; i < week.length; i++) {
+        calendarHtml += "<td>" + week[i] + "</td>"
+    }
+
+    for (let w = 0; w < 6; w++) {
+        calendarHtml += "<tr>"
+
+        for (let d = 0; d < 7; d++) {
+            if ((w == 0) & (d < firstDay)) {
+                // 一行目で、1日が始まる前の曜日
+                calendarHtml += "<td></td>" // 空白のマス
+            } else if (dayCount > lastDayCount) {
+                // 最後の行で、末日の後の曜日
+                calendarHtml += "<td></td>" // 空白のマス
+            } else {
+                calendarHtml += `<td>${dayCount}</td>`
+                dayCount++
+            }
+        }
+        calendarHtml += "</tr>"
+    }
+    calendarHtml += "</table>"
+
+    return calendarHtml
+}
+
+function drawCalendar(year, month) {
+    console.log("drawCalendar")
+    document.getElementById("calendar-title").textContent = creatCalendarTitle(
+        year,
+        month
+    )
+    document.querySelector("calendar-main").innerHTML = creatCalendar(
+        year,
+        month
+    )
+}
+
+function next() {
+    month++
+    if (month > 12) {
+        year++
+        month = 1
+    }
+    drawCalendar(year, month)
+}
+
+function prev() {
+    month--
+    if (month < 1) {
+        year--
+        month = 12
+    }
+    drawCalendar(year, month)
+}
+
+console.log("onload")
+// drawCalendar(year, month)
+// DOM要素をとるのは、<body>の中身が読まれた後でないといけない
